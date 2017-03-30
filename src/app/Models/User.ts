78037013df978a92e4d6a6890010b203a8df3465
Anthony Scinocco <scinocco.a@gmail.com>
2017-03-30 16:04:40 -0400

    added stock model and cleaned up user model

--- src/app/Models/User.ts
+++ src/app/Models/User.ts
@@ -1,24 +1,14 @@
 export class User
 {
-    private _id: string;
     public firstName: string;
     public lastName: string;
     public email: string;
     public password: string;
 
-    constructor(firstName: string, lastName: string, email: string, 
-                password?: string, _id?: string)
+    constructor(firstName: string, lastName: string, email: string)
     {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-
-        if (password) {
-            this.password = password;
-        }
-
-        if (_id) {
-            this._id = _id;
-        }
     }
 }
